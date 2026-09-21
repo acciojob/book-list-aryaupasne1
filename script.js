45d70751fb60048424d7efcc4527c4c46dc554e9
Arya Upasne <aryaupasne22@gmail.com>
2026-09-21 12:10:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const form = document.querySelector("#book-form");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const isbn = document.querySelector("#isbn");
+const bookList = document.querySelector("#book-list");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${title.value}</td>
+        <td>${author.value}</td>
+        <td>${isbn.value}</td>
+        <td>
+            <button class="delete">X</button>
+        </td>
+    `;
+
+    bookList.appendChild(row);
+
+    // Clear input fields
+    title.value = "";
+    author.value = "";
+    isbn.value = "";
+});
+
+// Delete book
+bookList.addEventListener("click", function (e) {
+    if (e.target.classList.contains("delete")) {
+        e.target.parentElement.parentElement.remove();
+    }
+});
